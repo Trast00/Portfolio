190306a669f9da96f54d5114246a357375ad2266
Allassane <dickoallassane1997@gmail.com>
2022-11-14 15:52:38 +0100

    add animation on menu Item when scroll through sections(optional)

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ closeMenu.addEventListener('click',function() {
   menu.style.right = "-100vw"
 });
 
+/* highlight displayed section */
+var workSection = document.getElementById("works")
+var aboutMeSection = document.getElementById("about-me")
+var footerSection = document.getElementById("footer")
+
+workSection.addEventListener('mouseover',function() {highlightMenuItem("works-desktop-menu-item", true)});
+aboutMeSection.addEventListener('mouseover',function() {highlightMenuItem("about-me-desktop-menu-item", true)});
+footerSection.addEventListener('mouseover',function() {highlightMenuItem("footer-desktop-menu-item", true)});
+
+workSection.addEventListener('mouseout',function() {highlightMenuItem("works-desktop-menu-item", false)});
+aboutMeSection.addEventListener('mouseout',function() {highlightMenuItem("about-me-desktop-menu-item", false)});
+footerSection.addEventListener('mouseout',function() {highlightMenuItem("footer-desktop-menu-item", false)});
+
+function highlightMenuItem(menuItemID, highlighted){
+  if(highlighted){
+    var elem = document.getElementById(menuItemID)
+    elem.style.fontSize = "1.2rem"
+    elem.style.textDecoration = "underline"
+  }else {
+    var elem = document.getElementById(menuItemID)
+    elem.style.fontSize = "0.938rem"
+    elem.style.textDecoration = "none"
+  }
+}
